7d66dd0fc0aac66eeb41deee64dbd37cbf3c0557
riyahann <riyahann1699@gmail.com>
2021-05-11 20:39:23 +0530

    Styling of login page

--- onlineinsurance/src/components/Login.js
+++ onlineinsurance/src/components/Login.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 import * as LoginAction from '../store/actions/LoginAction';
 import {bindActionCreators} from 'redux';
 import {Redirect} from 'react-router-dom';
+import './Login.css';
 
 class Login extends Component{
    constructor(){
@@ -51,26 +52,25 @@ class Login extends Component{
         
     }
     return(
-        <div>
-            
-            <div className="row">
-                        <div className="card col-md-6 offset-md-3 offset-md-3">
-                            <h2 className="text-center">Login</h2>
-                            <div className="card-body"></div>
-            <form>
-                <div className="form-group">
-                    
-                    <label>Username</label>
-                    <input type="text" name="emailId" className="form-control" value={this.state.emailId} onChange={this.onChange} required/><br></br>
-                    <label>Enter password</label>
-               <input type="password" name="password" className="form-control" value={this.state.password} onChange={this.onChange} required></input><br></br>
-                </div>
-                    <button className="btn btn-success" onClick={this.validation}>Login</button>
-          
-               
-            </form>
-            </div></div>
-            </div>
+        <div className="new">
+           <div class="box">
+  <h2>Login</h2>
+  <form>
+    <div class="inputBox">
+       <input type="text" name="emailId" className="form-control" value={this.state.emailId} onChange={this.onChange} required/><br></br>
+      <label>Username</label>
+    </div>
+    <div class="inputBox">
+      <input type="password" name="password" className="form-control" value={this.state.password} onChange={this.onChange} required></input><br></br>
+      <label>Password</label>
+      </div>
+      
+      <button className="btn btn-success" onClick={this.validation}>Login</button>
+    </form>
+    </div>
+   
+ </div>
+
     );
 }
 }
