0a4e329afc13635feed718385fe0974d1d40f436
riyahann <riyahann1699@gmail.com>
2021-05-13 16:00:51 +0530

    Reg cust

--- onlineinsurance/src/components/Welcome.js
+++ onlineinsurance/src/components/Welcome.js
@@ -18,6 +18,12 @@ render(){
 
       <div class="item active">
         <img src="images/Health-insurance-min_1.jpg" alt="Los Angeles"></img>
+        <div className="box-one">
+          <label>Login</label>
+        <p class="change_link">
+					<a href="/login" class="login-one"> Login</a>
+				</p>
+      </div>
         
       </div>
 
@@ -43,6 +49,59 @@ render(){
       <span class="sr-only">Next</span>
     </a>
   </div>
+  <section id="Services" class="content-section text-center">
+            <div class="container">
+              <div class="block-heading">
+                <h2>What We Offer</h2>
+              </div>
+              <div class="row">
+                <div class="col-md-3 col-sm-6">
+                 
+                      <div class="front-content">
+                        <i class="fa fa-hotel fa-5x "  aria-hidden="true"></i>
+                        <h3>Hospitalisation</h3>
+                      
+                    </div>
+                    <div class="service-content">
+                      <p>Medical expenses incurred during hospitalisation for more than 24 hours, including room charges, doctor/surgeon’s fee, medicine bills, etc.</p>
+                    </div>
+                  
+                </div>
+                <div class="col-md-3 col-sm-6">
+                  
+                      <div class="front-content">
+                        <i class="fa fa-ambulance fa-5x"></i>
+                        <h3>Ambulance costs</h3>
+                      </div>
+                   
+                    <div class="service-content">
+                     
+                      <p> Ambulance charges and other transportation costs of the person insured are also covered under health insurance policies</p>
+                    </div>
+                 
+                </div>
+                <div class="col-md-3 col-sm-6">
+                  
+                      <div class="front-content">
+                        <i class="fa fa-stethoscope fa-5x" aria-hidden="true"></i>
+                        <h3>Medical check-ups</h3>
+                  </div>
+                    <div class="service-content">
+                      <p>Our policies also offer coverage for periodic health check-ups. We offer free check-ups on the basis of earlier no-claim bonuses</p>
+                    </div>  
+                </div>
+                <div class="col-md-3 col-sm-6">
+                      <div class="front-content">
+                        <i class="fa fa-money fa-5x"></i>
+                        <h3>Fixed premiums</h3>
+                      </div>
+                    <div class="service-content">
+                      <p>We offer higher coverage for premiums that are affordable. Premium amounts also remain fixed for the entire duration of the policy</p>
+                    </div>
+                  </div>
+               
+              </div>
+            </div></section>
 </div>
 
       
