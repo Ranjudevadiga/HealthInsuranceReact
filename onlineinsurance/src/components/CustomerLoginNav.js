fa39e903fe8e3020a1a9bda1a6f12d9de896dc15
riyahann <riyahann1699@gmail.com>
2021-05-18 20:41:36 +0530

    change in nav

--- onlineinsurance/src/components/CustomerLoginNav.js
+++ onlineinsurance/src/components/CustomerLoginNav.js
@@ -3,7 +3,7 @@ import{Navbar,Nav,NavDropdown} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 export default class HomeNavBar extends Component{
     logout(){
-        sessionStorage.removeItem("customerId");
+        sessionStorage.removeItem("userId");
         window.location.href="/login";
     }
     render(){
